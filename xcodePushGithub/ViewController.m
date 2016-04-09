b99b91b07a9c32dd5aea33e66e6c4d877f20e29b
jcy001 <2669448208@qq.com>
2016-04-09 20:12:03 +0800

    修改了ViewController

--- xcodePushGithub/ViewController.m
+++ xcodePushGithub/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"从Xcode推送到远程仓库");
 }
 
 - (void)didReceiveMemoryWarning {
